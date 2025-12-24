15b077d50ccccd678828641ade95321677e444c8
hamidarki <hamidarki0@gmail.com>
2025-12-24 12:16:22 +0700

    membuat bottom navigation

---
+++ lib/pages/kelas_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KelasPage extends StatelessWidget {
+  const KelasPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Kelas Page')));
+  }
+}
